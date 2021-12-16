5d018aca0ab4535025116dfd8b0a3d9f1a3729f0
Sukrish Gokulakrishnan <enderslash@outlook.com>
2021-12-16 15:56:31 +0530

    Enemies bounce off both walls, bunkers added (indestructible)
    Also changed spawnline box collider to is trigger, and also same for right and left wall. Added box colliders to bunkers as well.

--- Assets/Scripts/DirectionChangeLeftWall.cs
+++ Assets/Scripts/DirectionChangeLeftWall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirectionChange : MonoBehaviour
+public class DirectionChangeLeftWall : MonoBehaviour
 {
     private EnemyMover enemyMoverScript;
 
@@ -24,20 +24,20 @@ public class DirectionChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("EnemyBlock"))
         {
-            other.gameObject.transform.position = other.gameObject.transform.position + new Vector3(-1, -0.5f, 0);
-            enemyMoverScript.direction = 'r';
+            other.gameObject.transform.position = other.gameObject.transform.position + new Vector3(1, -0.5f, 0);
+            enemyMoverScript.direction = 'l';
         }
         else if (other.gameObject.CompareTag("LastEnemyBlock"))
         {
-            other.gameObject.transform.position = other.gameObject.transform.position + new Vector3(-1, -0.5f, 0);
-            enemyMoverScript.direction = 'l';
+            other.gameObject.transform.position = other.gameObject.transform.position + new Vector3(1, -0.5f, 0);
+            enemyMoverScript.direction = 'r';
         }
     }
 }
